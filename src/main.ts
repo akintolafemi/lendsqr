94d8f9732b08c216ad9874f9d3951155dd6b8865
micheal <michealakintola106@yahoo.com>
2024-10-28 16:49:52 +0100

    Feature: 1. Installed knex ORM for DB operation and mysql2 for knex 2. Updated app configuration for proper error handling using class-validator and class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,52 @@
-import { NestFactory } from '@nestjs/core';
+import {
+  BadRequestException,
+  Logger,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { AppOptions } from '@utils/app.options.util';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    AppOptions,
+  );
+
+  app.setGlobalPrefix('/api/v1');
+
+  app.set('trust proxy', true);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        return new BadRequestException({
+          statusText: 'bad request',
+          status: 400,
+          message:
+            errors[0]?.children[0]?.children[0]?.constraints[
+              Object?.keys(errors[0]?.children[0]?.children[0]?.constraints)[0]
+            ] ||
+            errors[0]?.children[0]?.constraints[
+              Object?.keys(errors[0]?.children[0]?.constraints)[0]
+            ] ||
+            errors[0]?.constraints[Object?.keys(errors[0]?.constraints)[0]] ||
+            'Unable to validate request',
+        });
+      },
+    }),
+  );
+
+  app.enableShutdownHooks();
+
+  const logger = new Logger(NestApplication.name);
+  const port = process.env.PORT || 3005;
+  await app.listen(port, () => {
+    logger.log(`Server is now listening on port ${port}`);
+  });
 }
 bootstrap();
